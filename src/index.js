3dec2ed16117c764aa6b6ca58c97798a8350cec1
Adonisiy <adonisiy@list.ru>
2024-07-29 16:53:10 +0400

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  let str = n.toString();
+  if (str[0] == '-'){
+    str = str.slice(1);
+  }
+  return +str.toString().split('').reverse().join('');
 }
